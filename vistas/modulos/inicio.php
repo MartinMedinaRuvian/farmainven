b2188bc74430c47d6a29a74768ddf960d22cea08
MartinMedinaRuvian <martindjmedina@hotmail.com>
2021-11-26 14:31:22 -0500

    Version 1.0

---
+++ vistas/modulos/inicio.php
@@ -0,0 +1,19 @@
+<div class="content-wrapper">
+
+  <section class="content-header">
+    
+    <h1>
+      
+      Tablero
+      
+      <small>Panel de Control</small>
+    
+    </h1>
+
+    <h1>Próximamente</h1>
+
+  </section>
+
+
+ 
+</div>
